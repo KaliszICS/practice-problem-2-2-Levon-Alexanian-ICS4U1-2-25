44439b867fc2f7498ec39535834386534d8603a6
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-27 05:59:26 +0000

    Update PracticeProblem.java

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
   
   Your function should:
   
-  Search for the number using both a sequential search and a binary search.</br>
+  Search for the number uing both a sequential search and a binary search.</br>
   Return how many loops your searches took to find the given number for each search in an integer array.</br>
-  Index 0 of the array is the sequential number of loops and index 1 of the array is the binary number of loops.
+  Index 0 of the array is the sequential numsber of loops and index 1 of the array is the binary number of loops.
   
   2. Create another function called compareStringSearch(String[] array, String word) that searches an array for a word.
   Your program should do the exact same actions as question 1 but on Strings instead of integers.
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,50 @@
+import java.util.Arrays;
 public class PracticeProblem {
-
-	public static void main(String args[]) {
-		
-	}
-	
+	public static int[] compareSearch(int[] array, int number) {
+        int sequentialloops = 0;
+        for (int i = 0; i < array.length; i++) {
+            sequentialloops++;
+            if (array[i] == number) {
+                break;
+            }
+        }
+		int binaryloops = 0;
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            binaryloops++;
+            int mid = left + (right - left) / 2;
+            if (array[mid] == number) {
+				break;
+            } else if (array[mid] < number) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+		return new int[]{sequentialloops, binaryloops};
+    }
+    public static int[] compareStringSearch(String [] array, String word) {
+        int sequentialloops = 0;
+        for (int i = 0; i < array.length; i++) {
+            sequentialloops++;
+            if (array[i] == word) {
+                break;
+            }
+        }
+		int binaryloops = 0;
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            binaryloops++;
+            int mid = left + (right - left) / 2;
+            if (array[mid] == word) {
+				break;
+            } else if (array[mid].compareTo(word) < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+		return new int[]{sequentialloops, binaryloops};
+    }
+	// They're villagers ... love crushing loaf. Do you love crushing loaf Mr. Kalisz????????
 }
